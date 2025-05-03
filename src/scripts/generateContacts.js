806c6634b831194c8c03b0404bba73ebd62f1bf6
Imojenga <aina.jenko@gmail.com>
2025-05-03 21:25:54 +0300

    add all

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,20 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const generateContacts = async (number) => {
+  try {
+    const prevContacts = await readContacts();
+    let contacts = [];
+    for (let i = 0; i < number; i++) {
+      const data = createFakeContact();
+      contacts.push(data);
+    }
+    await writeContacts([...prevContacts, ...contacts]);
+    return contacts;
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 generateContacts(5);
